7d8930a5ec8019dca1e30b4bda137bd06b44facd
bangchan <chandrakusumaa133@gmail.com>
2024-11-13 00:58:24 +0800

    rename root

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import TodoRoutes from "./routes/TodoRoutes";
 import RecordRoutes from "./routes/RecordRoutes";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
