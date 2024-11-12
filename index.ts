4143f7058742d0e5bd2ca0531b924568e64f542a
bangchan <chandrakusumaa133@gmail.com>
2024-11-13 02:11:35 +0800

    fix

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import TodoRoutes from "./src/routes/TodoRoutes";
 import RecordRoutes from "./src/routes/RecordRoutes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
